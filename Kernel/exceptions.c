669b206bfeb4d99be717c8d0ae987535cae2ec0e
Juan Cruz Bafico <86856551+jBafico@users.noreply.github.com>
2022-05-26 19:46:55 -0300

    Mejorado el proceso de excepciones

--- Kernel/exceptions.c
+++ Kernel/exceptions.c
@@ -1,49 +1,51 @@
 #include "naiveConsole.h"
 #include "stdint.h"
 #include "lib.h"
+#include "interrupts.h"
 
 #define ZERO_EXCEPTION_ID 0
 #define INVALID_OPCODE 6
 
+typedef void (*Exception)(void);
+
 char* regNameArr[]={"rax: ", "rbx: ", "rcx: ", "rdx: ", "rbp: ", "rsi: ", "rdi: ", "rsp: ", "r8: ","r9: ","r10: ","r11: ","r12: ","r13: ","r14: ","r15: "};
 
+static Exception exceptions[]={&zeroDivision, 0, 0, 0, 0, 0, &invalidOpcode};
+
 static void zeroDivision() {
-
-    //TODO manejar corte de programa
     char * errMsg= "Error: Program tried to devide by zero. Stopping process.";
-    ncPrintAttribute(errMsg, Red, Black); //TODO Revisar como queremos hacer que imprima la salida de error
-
-	// Handler para manejar excepcíon
-    //TODO aca poner la funcion que imprime los estados de registros
-    uint64_t * regArr= prepareRegisters(); // revisar si esta funcion funciona correctamente
-
-    for (int i = 0; i < 16; ++i) {
-        ncPrint(regNameArr[i]);
-        ncPrintHex(regArr[i]);
-    }
+    exceptionHandler(errMsg);
 }
 
 static void invalidOpcode() {
-    //TODO manejar corte de programa
     char * errMsg= "Error: Invalid operation code. Stopping process.";
-    ncPrintAttribute(errMsg, Red, Black); //TODO Revisar como queremos hacer que imprima la salida de error
-    // Handler para manejar excepcíon
+    exceptionHandler(errMsg);
+}
 
+void exceptionHandler(char * errMsg){
+    ncPrintAttribute(errMsg, Red, Black); //TODO Revisar como queremos hacer que imprima la salida de error
+    ncNewline();
     //TODO poner la funcion que imprime los estados de registros
     uint64_t * regArr= prepareRegisters();
     for (int i = 0; i < 16; ++i) {
         ncPrint(regNameArr[i]);
         ncPrintHex(regArr[i]);
     }
+    ncNewline();
+    //Seccion de frenado de Procesos
+    ncPrintAttribute("Ingrese ENTER para poder continuar", Red, Black);
+    int c;
+    do{
+        _hlt();//hlt frena el CPU hasta que se detecte la proxima interrupcion externa
+    }while((c=getChar()) != '\n');//TODO revisar de tener una funcion que haga lo del sys_read
+    ncClear();
+    //aca hay que volver a darle el control al usuario
+
 }
 
 void exceptionDispatcher(int exception) {
-    switch (exception) {
-        case ZERO_EXCEPTION_ID:
-            zeroDivision();
-            break;
-        case INVALID_OPCODE:
-            invalidOpcode();
-            break;
-    }
+    Exception exFunc = exceptions[exception];
+	if(exFunc != 0){
+		exFunc();
+	}
 }
